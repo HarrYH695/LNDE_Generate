4dea2d0d00b97474a7bd7de5b871b0a141bed09d
hanhy <hanhy@umich.edu>
2025-03-07 18:43:00 -0500

    add variables info

--- z_try.py
+++ z_try.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-# TIME_BUFF: [idx - history_length, idx)时间段内的车辆位置信息,TIME_BUFF[i]代表i时刻的所有车辆位置，具体请看readme中的data format。
+# TIME_BUFF: [idx - history_length, idx)时间段内的车辆位置信息,TIME_BUFF[i]代表i时刻的所有车辆的相关信息，具体请看readme中的data format。
 # 注意TIME_BUFF已经被去除了out of bound的车以及可能的crash
 
 #
